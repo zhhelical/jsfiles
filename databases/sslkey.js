2604ad4164564f20d4676d1155aeeefba56d4be1
zhhelical <zhhelical@163.com>
2019-09-12 19:34:28 +0800

    删除了

--- databases/sslkey.js
+++
@@ -1,6 +0,0 @@
-//sslkey.js
-var fs = require('fs')
-module.exports = {
-    key: fs.readFileSync(__dirname+'/2_www.helicalzh.la.key'),
-    cert: fs.readFileSync(__dirname+'/1_www.helicalzh.la_bundle.crt')
-}
